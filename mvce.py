b8f99a68ac37aafb890dfbe6b8f83ee300ac2063
John T. Wodder II <git@varonathe.org>
2022-10-24 19:28:15 -0400

    More fiddling

--- mvce.py
+++ mvce.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 import json
 import logging
+from pathlib import Path
 import subprocess
 import tempfile
 
 URL = "https://httpbin.org/encoding/utf8"
-FILE = "foo/bar.txt"
+FILE = Path("foo", "bar.txt")
 
 logging.basicConfig(
     format="%(asctime)s [%(levelname)-8s] %(name)s: %(message)s",
@@ -25,12 +26,12 @@ subprocess.run(["git-annex", "init"], cwd=repo, check=True)
 
 log.info("Downloading a file to %s ...", FILE)
 log.debug("Running: git-annex addurl --file %s %s", URL, FILE)
-subprocess.run(["git-annex", "addurl", "--file", FILE, URL], cwd=repo, check=True)
+subprocess.run(["git-annex", "addurl", "--file", str(FILE), URL], cwd=repo, check=True)
 
 log.info("Setting file metadata via batch mode ...")
 log.debug("Running: git-annex metadata --batch --json --json-error-messages")
 
-line_in = (json.dumps({"file": FILE, "fields": {"foo": ["bar"]}}) + "\r\n").encode("utf-8")
+line_in = (json.dumps({"file": str(FILE), "fields": {"foo": ["bar"]}}) + "\r\n").encode("utf-8")
 log.debug("Input: %r", line_in)
 r = subprocess.run(["git-annex", "metadata", "--batch", "--json", "--json-error-messages"], cwd=repo, input=line_in, stdout=subprocess.PIPE, check=True)
 log.debug("Output: %r", r.stdout)
