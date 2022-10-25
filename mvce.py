b79b7db294974e9094b2605e65a9417120f8c68d
John T. Wodder II <git@varonathe.org>
2022-10-25 10:46:05 -0400

    Go back to not reading everying from addurl before calling metadata

--- mvce.py
+++ mvce.py
@@ -57,8 +57,17 @@ async def amain():
             log.debug("Input to addurl: %r", line_in)
             await addurl.stdin.send(line_in)
         await addurl.stdin.aclose()
+
+        buf = b""
         async for out in addurl.stdout:
             log.debug("Output chunk from addurl: %r", out)
+            buf += out
+            if b'\n' in out:
+                break
+
+        s = buf[:buf.index(b"\n")].decode("utf-8")
+        file = json.loads(s)["file"]
+        metadata = FILES[file]["metadata"]
 
         log.info("Setting file metadata via batch mode ...")
         log.debug(
@@ -70,8 +79,6 @@ async def amain():
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
         ) as p:
-            file = "foo/bar.txt"
-            metadata = FILES[file]["metadata"]
             line_in = (
                 json.dumps(
                     {"file": file, "fields": metadata}, separators=(",", ":")
@@ -83,6 +90,9 @@ async def amain():
             p.stdin.flush()
             line_out = p.stdout.readline()
             log.debug("Output from metadata: %r", line_out)
+
+        async for out in addurl.stdout:
+            log.debug("Output chunk from addurl: %r", out)
 
 
 if __name__ == "__main__":
